e36371075df109a0cec9539021e05c3d99979210
Crowdbotics <team@crowdbotics.com>
2020-11-09 19:33:37 +0000

    Update models

--- the_gammy_game_22447/wsgi.py
+++ the_gammy_game_22447/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_gammy_game_22447.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_gammy_game_22447.settings")
 
 application = get_wsgi_application()
